afdcba108dfcd96ed56e5aaa9e9b9a734e47b4c4
WideWorldWill <142928659+WideWorldWill@users.noreply.github.com>
2023-11-12 09:45:56 -0500

    Update cipher.py

--- cipher.py
+++ cipher.py
@@ -1,1 +1,14 @@
-# add your code here
+caesar = input("Enter Message:")
+dressing = int(input("Enter Key:"))
+salad = ""
+for i in range(len(caesar)):
+  #Using ascii to convert characters into their numerical equivalents
+  ascii = ord(caesar[i])
+  if ascii >= 65 and ascii <= 90:
+    #using chr to convert ascii number back to it's character equivalent
+    salad += chr((ascii + dressing - 65) % 26 + 65)
+  elif ascii >= 97 and ascii <= 122:
+    salad += chr((ascii + dressing - 97) % 26 + 97)
+  else:
+    salad += caesar[i]
+print(salad)# add your code here
