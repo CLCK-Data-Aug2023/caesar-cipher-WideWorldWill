07fb1440cd03ed0cecebc217339ad7bf3838793c
WideWorldWill <142928659+WideWorldWill@users.noreply.github.com>
2023-11-12 14:23:06 -0500

    Update cipher.py

--- cipher.py
+++ cipher.py
@@ -1,22 +1,17 @@
-caesar = input("enter message:")
+caesar = input("enter message: ")
 dressing = 5
-caesar=caesar.lower()
+
 salad = ""
-words = caesar.split() # Split the input message into words
-
-for word in words: # Encrypt each word separately
-  new_word = ""
-  for char in word:
-    if char.isalpha():
-      ascii_code = ord(char)
-      if ascii_code >= 65 and ascii_code <= 90:
-        new_ascii_code = (ascii_code + dressing) % 91
-      else:
-        new_ascii_code = (ascii_code + dressing) % 123
-      new_char = chr(new_ascii_code)
-      new_word += new_char
+for char in caesar:
+  if char.isalpha():
+    ascii_code = ord(char)
+    if ascii_code >= 65 and ascii_code <= 90:
+      new_ascii_code = (ascii_code + dressing) % 91
     else:
-      new_word += char
-  salad += new_word + " "
-
+      new_ascii_code = (ascii_code + dressing) % 123
+    new_char = chr(new_ascii_code)
+    salad += new_char
+  else:
+    salad += char
+salad=salad.lower()
 print(salad)
