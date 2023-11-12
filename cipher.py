01cb28fe48b1dfd3c584da989b7ec12f608169ee
WideWorldWill <142928659+WideWorldWill@users.noreply.github.com>
2023-11-12 15:02:25 -0500

    Update cipher.py

--- cipher.py
+++ cipher.py
@@ -4,13 +4,13 @@ dressing = 5
 salad = ""
 alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-for char in message:
+for char in caesar:
   if char in alphabet:
     index = alphabet.index(char)
     new_index = (index + shift) % len(alphabet)
     new_char = alphabet[new_index]
     salad += new_char
   else:
-    encrypted_message += char
+    salad += char
 salad=salad.lower()
 print("The encrypted sentence is:", salad)
