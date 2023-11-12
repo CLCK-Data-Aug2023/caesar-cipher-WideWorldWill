a29e0aa8a0c3c11e3b8a3a9315b57115a6cac258
WideWorldWill <142928659+WideWorldWill@users.noreply.github.com>
2023-11-12 14:03:09 -0500

    Update cipher.py

--- cipher.py
+++ cipher.py
@@ -1,16 +1,22 @@
-caesar = input("Please enter a senctence: ")
+caesar = input("enter message: ")
 dressing = 5
 
 salad = ""
-for char in caesar:
-  if char.isalpha():
-    ascii_code = ord(char)
-    if ascii_code >= 65 and ascii_code <= 90:
-      new_ascii_code = (ascii_code + dressing) % 91
+words = caesar.split() # Split the input message into words
+
+for word in words: # Encrypt each word separately
+  new_word = ""
+  for char in word:
+    if char.isalpha():
+      ascii_code = ord(char)
+      if ascii_code >= 65 and ascii_code <= 90:
+        new_ascii_code = (ascii_code + dressing) % 91
+      else:
+        new_ascii_code = (ascii_code + dressing) % 123
+      new_char = chr(new_ascii_code)
+      new_word += new_char
     else:
-      new_ascii_code = (ascii_code + dressing) % 123
-    new_char = chr(new_ascii_code)
-    salad += new_char
-  else:
-    salad += char
-print("The encrypted sentence is: ", salad)
+      new_word += char
+  salad += new_word + " "
+
+print(salad)
